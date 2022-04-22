ade4b439a40b25bf581df506435666f2f3df3c0e
Steviexd <steven.newman2172@stu.mtec.edu>
2022-04-21 18:11:03 -0600

    W6D2

---
+++ index.js
@@ -0,0 +1,126 @@
+var box1 = $("#box1")
+var box2 = $("#box2")
+var box3 = $("#box3")
+var box4 = $("#box4")
+var box5 = $("#box5")
+var box6 = $("#box6")
+var box7 = $("#box7")
+var box8 = $("#box8")
+var box9 = $("#box9")
+
+$(document).ready(function () {
+
+
+        if($("#box1").click(function() {
+
+console.log("click")
+
+        if($("#box1").hasClass("light")) {
+            ($("#box1").removeClass("light"))
+            ($("#box1").addClass("dark"))
+         }else if($("#box1").hasClass ("dark")) {
+            ($("#box1").removeClass("dark"))
+            ($("#box1").addClass("light"))
+         }
+        }));
+    
+
+        if($("#box2").click(function() {
+
+
+        if($("#box2").hasClass("light")) {
+            ($("#box2").removeClass("light"))
+            ($("#box2").addClass("dark"))
+         }else if($("#box2").hasClass ("dark")) {
+            ($("#box2").removeClass("dark"))
+            ($("#box2").addClass("light"))
+         }
+        }));
+
+        if($("#box3").click(function() {
+
+
+        if($("#box3").hasClass("light")) {
+            ($("#box3").removeClass("light"))
+            ($("#box3").addClass("dark"))
+         }else if($("#box3").hasClass ("dark")) {
+            ($("#box3").removeClass("dark"))
+            ($("#box3").addClass("light"))
+         }
+        }));
+    
+        if($("#box4").click(function() {
+
+
+            if($("#box4").hasClass("light")) {
+                ($("#box4").removeClass("light"))
+                ($("#box4").addClass("dark"))
+             }else if($("#box4").hasClass ("dark")) {
+                ($("#box4").removeClass("dark"))
+                ($("#box4").addClass("light"))
+             }
+            }));
+
+        if($("#box5").click(function() {
+
+
+        if($("#box5").hasClass("light")) {
+            ($("#box5").removeClass("light"))
+            ($("#box5").addClass("dark"))
+         }else if($("#box5").hasClass ("dark")) {
+            ($("#box5").removeClass("dark"))
+            ($("#box5").addClass("light"))
+         }
+        }));
+
+    
+        if($("#box6").click(function() {
+
+
+        if($("#box6").hasClass("light")) {
+            ($("#box6").removeClass("light"))
+            ($("#box6").addClass("dark"))
+         }else if($("#box6").hasClass ("dark")) {
+            ($("#box6").removeClass("dark"))
+            ($("#box6").addClass("light"))
+         }
+        }));
+
+
+        if($("#box7").click(function() {
+
+
+        if($("#box7").hasClass("light")) {
+            ($("#box7").removeClass("light"))
+            ($("#box7").addClass("dark"))
+         }else if($("#box7").hasClass ("dark")) {
+            ($("#box7").removeClass("dark"))
+            ($("#box7").addClass("light"))
+         }
+        }));
+
+        if($("#box8").click(function() {
+
+
+        if($("#box8").hasClass("light")) {
+            ($("#box8").removeClass("light"))
+            ($("#box8").addClass("dark"))
+         }else if($("#box8").hasClass ("dark")) {
+            ($("#box8").removeClass("dark"))
+            ($("#box8").addClass("light"))
+         }
+        }));
+
+        if($("#box9").click(function() {
+
+
+        if($("#box9").hasClass("light")) {
+            ($("#box9").removeClass("light"))
+            ($("#box9").addClass("dark"))
+         }else if($("#box9").hasClass ("dark")) {
+            ($("#box9").removeClass("dark"))
+            ($("#box9").addClass("light"))
+         }
+        }));
+    });
+
